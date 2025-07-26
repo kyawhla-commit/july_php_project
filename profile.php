de5696069ae9d25b70a8e9bc177af3b3859724e8
Khun Tu Pi <bwarpay.bp8@gmail.com>
2025-07-26 10:52:44 +0630

    create profile

---
+++ profile.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container" style="max-width: 800px">
+        <h1 class="h3 my-4">Profile</h1>
+        <ul class="list-group mb-3">
+            <li class="list-group-item">Name: Alice</li>
+            <li class="list-group-item">Email: alice@gmail.com</li>
+            <li class="list-group-item">Phone: 545454541</li>
+            <li class="list-group-item">Address: Some Address</li>
+        </ul>
+        <a href="_actions/logout.php" class="text-danger">Logout</a>
+    </div>
+</body>
+</html>
