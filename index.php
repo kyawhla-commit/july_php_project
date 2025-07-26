de559806207bb6c350f3870266c3352bf63c9c9c
Khun Tu Pi <bwarpay.bp8@gmail.com>
2025-07-26 11:25:30 +0630

    add login logout system

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container text-center" style="max-width: 600px">
+        <h1 class="mb-3">Login</h1>
+
+        <?php if(isset($_GET['incorrect'])): ?>
+            <div class="alert alert-warning">
+                Incorrect email or password
+            </div>
+        <?php endif ?>
+        <form action="_actions/login.php" method="post" class="mb-3">
+            <input type="email" name="email" placeholder="Email" class="form-control mb-2" require>
+            <input type="password" name="password" placeholder="Password" class="form-control mb-2" require>
+            <button class="btn btn-primary w-100">Login</button>
+        </form>
+        <a href="register.php">Register</a>
+    </div>
+</body>
+</html>
--- profile.php
+++ profile.php
@@ -1,11 +1,21 @@
+<?php
+    session_start();
+    
+    if(!isset($_SESSION['user'])) {
+        header("location: index.php");
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container" style="max-width: 800px">
         <h1 class="h3 my-4">Profile</h1>
@@ -18,4 +28,5 @@
         <a href="_actions/logout.php" class="text-danger">Logout</a>
     </div>
 </body>
+
 </html>
